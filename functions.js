4457175b1d3bee2f23ef0b67bd26caaccfba9a85
FelipeFidelisA <146492055+FelipeFidelisA@users.noreply.github.com>
2024-08-22 11:14:26 -0300

    Verifica se tb é primo

--- functions.js
+++ functions.js
@@ -1,7 +1,21 @@
 function verificarParOuImpar() {
     var numero = Number(document.getElementById('numero').value);
 
-    var resultado = (numero % 2 === 0) ? 'O seu numero é impar.' : 'O seu numero é par.'; //Verifica o número passado se ele vai ser impar ou par . 
+    var resultado = (numero % 2 == 0) ? 'O seu numero é par.' : 'O seu numero é impar.' ; //Verifica o número passado se ele vai ser impar ou par . 
     
     document.getElementById('resultado').innerHTML = resultado;
+}
+
+function verificarNumeroPrimo() {
+    var numero = parseInt(document.getElementById('numPrimo').value);
+    var divisores = 0;
+
+    for (var count = 1; count <= numero; count++) {
+        if (numero % count === 0) {
+            divisores++;
+        }
+    }
+
+    var resposta = (divisores === 2) ? 'O seu número é primo.' : 'O número é composto.';
+    document.getElementById('resposta').innerHTML = resposta;
 }
